0d5ddbc2e25ad984b2f14b75ad65cd2efe04d7f2
leehj04 <100088909+leehj04@users.noreply.github.com>
2023-04-26 00:33:58 +0900

    CMApp

--- CMApp/src/main/java/CMClientEventHandler.java
+++ CMApp/src/main/java/CMClientEventHandler.java
@@ -1,11 +1,40 @@
+import kr.ac.konkuk.ccslab.cm.event.CMEvent;
+import kr.ac.konkuk.ccslab.cm.event.CMSessionEvent;
+import kr.ac.konkuk.ccslab.cm.event.handler.CMAppEventHandler;
 import kr.ac.konkuk.ccslab.cm.stub.CMClientStub;
+import kr.ac.konkuk.ccslab.cm.info.CMInfo;
 import kr.ac.konkuk.ccslab.cm.stub.CMServerStub;
 
-public class CMClientEventHandler {
+public class CMClientEventHandler implements CMAppEventHandler {
     private CMClientStub m_clientStub;
 
     public CMClientEventHandler(CMClientStub clientStub)
     {
         m_clientStub = clientStub;
+    }
+
+    @Override
+    public void processEvent(CMEvent cme){
+        switch(cme.getType())
+        {
+            case CMInfo.CM_SESSION_EVENT:
+                processSessionEvent(cme);
+                break;
+            default:
+                return;
+        }
+    }
+
+    private void processSessionEvent(CMEvent cme)
+    {
+        CMSessionEvent se = (CMSessionEvent) cme;
+        switch (se.getID())
+        {
+            case CMSessionEvent.LOGIN:
+                System.out.println("["+se.getUserName()+"] requests login.");
+                break;
+            default:
+                return;
+        }
     }
 }
