3bacf29ec14e72ff2958ccb24f9078057770b998
leehj04 <100088909+leehj04@users.noreply.github.com>
2023-04-25 23:37:23 +0900

    CMApp

---
+++ CMApp/src/main/java/CMClientEventHandler.java
@@ -0,0 +1,11 @@
+import kr.ac.konkuk.ccslab.cm.stub.CMClientStub;
+import kr.ac.konkuk.ccslab.cm.stub.CMServerStub;
+
+public class CMClientEventHandler {
+    private CMClientStub m_clientStub;
+
+    public CMClientEventHandler(CMClientStub clientStub)
+    {
+        m_clientStub = clientStub;
+    }
+}
---
+++ CMApp/src/main/java/CMServerEventHandler.java
@@ -0,0 +1,40 @@
+import kr.ac.konkuk.ccslab.cm.*;
+import kr.ac.konkuk.ccslab.cm.event.CMEvent;
+import kr.ac.konkuk.ccslab.cm.event.CMSessionEvent;
+import kr.ac.konkuk.ccslab.cm.event.handler.CMAppEventHandler;
+import kr.ac.konkuk.ccslab.cm.stub.CMServerStub;
+import kr.ac.konkuk.ccslab.cm.info.CMInfo;
+
+public class CMServerEventHandler implements CMAppEventHandler {
+    private CMServerStub m_serverStub;
+
+    public CMServerEventHandler(CMServerStub serverStub)
+    {
+        m_serverStub = serverStub;
+    }
+
+    @Override
+    public void processEvent(CMEvent cme) {
+        switch (cme.getType())
+        {
+            case CMInfo.CM_SESSION_EVENT:
+                processSessionEvent(cme);
+                break;
+            default:
+                return;
+        }
+    }
+
+    private void processSessionEvent(CMEvent cme)
+    {
+        CMSessionEvent se = (CMSessionEvent) cme;
+        switch (se.getID())
+        {
+            case CMSessionEvent.LOGIN:
+                System.out.println("["+se.getUserName()+"] requests login.");
+                break;
+            default:
+                return;
+        }
+    }
+}
